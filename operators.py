a5fd856d15f6e86db549768ec1cadb7998383f75
Eric Doan <103901993+E-Doan@users.noreply.github.com>
2023-08-30 03:00:00 -0400

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,8 @@
+# Get input from the user
+number = int(input("Enter a number: "))
+
+# Check if the number is greater than 0 and less than 100
+result = (number > 0) and (number < 100)
+
+# Display the result
+print("Number is greater than 0 and less than 100:", result)
