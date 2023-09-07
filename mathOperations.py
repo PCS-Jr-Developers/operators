ae53f50b3ffd6a67e140593610ecbbd5bc43de1a
Eric Doan <103901993+E-Doan@users.noreply.github.com>
2023-09-06 22:57:24 -0400

    Create mathOperations.py

---
+++ mathOperations.py
@@ -0,0 +1,27 @@
+# Addition (+)
+result_add = 3 + 5
+print("3 + 5 =", result_add)
+
+# Subtraction (-)
+result_subtract = 10 - 7
+print("10 - 7 =", result_subtract)
+
+# Multiplication (*)
+result_multiply = 4 * 6
+print("4 * 6 =", result_multiply)
+
+# Division (/)
+result_divide = 15 / 3
+print("15 / 3 =", result_divide)
+
+# Integer Division (//)
+result_int_divide = 15 // 3
+print("15 // 3 =", result_int_divide)
+
+# Modulus (%)
+result_modulus = 17 % 5
+print("17 % 5 =", result_modulus)
+
+# Exponentiation (**)
+result_exponent = 2 ** 3
+print("2 ** 3 =", result_exponent)
