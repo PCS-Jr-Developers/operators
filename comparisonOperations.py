9f9170c22d3bb8766143af34f34513ba055cd278
Eric Doan <103901993+E-Doan@users.noreply.github.com>
2023-09-06 22:54:59 -0400

    Create comparisonOperations.py

---
+++ comparisonOperations.py
@@ -0,0 +1,23 @@
+# Equal (==)
+result_equal = 5 == 5  # Is 5 the same as 5?
+print(result_equal)  # Output: True
+
+# Not Equal (!=)
+result_not_equal = 5 != 3  # Is 5 different from 3?
+print(result_not_equal)  # Output: True
+
+# Greater Than (>)
+result_greater_than = 10 > 7  # Is 10 bigger than 7?
+print(result_greater_than)  # Output: True
+
+# Less Than (<)
+result_less_than = 3 < 8  # Is 3 smaller than 8?
+print(result_less_than)  # Output: True
+
+# Greater Than or Equal To (>=)
+result_greater_equal = 5 >= 5  # Is 5 bigger than or equal to 5?
+print(result_greater_equal)  # Output: True
+
+# Less Than or Equal To (<=)
+result_less_equal = 12 <= 15  # Is 12 smaller than or equal to 15?
+print(result_less_equal)  # Output: True
