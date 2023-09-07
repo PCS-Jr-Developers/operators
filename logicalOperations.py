f482814281b7b15ce9f0d6a38f4785b0b1b9a0de
Eric Doan <103901993+E-Doan@users.noreply.github.com>
2023-09-06 22:56:28 -0400

    Create logicalOperations.py

---
+++ logicalOperations.py
@@ -0,0 +1,13 @@
+x = True
+y = False
+result = x and y
+print(result)  # Output: False
+
+x = True
+y = False
+result = x or y
+print(result)  # Output: True
+
+x = True
+result = not x
+print(result)  # Output: False
